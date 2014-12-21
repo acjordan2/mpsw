f5e660d4522ea08ff78cec58aa5dde3891686081
Andrew Jordan <acjordan2@gmail.com>
2014-12-21 00:36:40 -0600

    Added dynamic memory allocation for the results buffer, added support for multiple columns, fixed segfault

--- stmt_prepare.c
+++ stmt_prepare.c
@@ -5,10 +5,15 @@
 #include <mysql/mysql.h>
 
 #define STRING_SIZE 256
+#define DB_HOST  ""
+#define DB_USER  ""
+#define DB_PASS  ""
+#define DB_NAME  ""
 
-int stmt_prepare(MYSQL *mysql, MYSQL_STMT *stmt, MYSQL_BIND *param, MYSQL_BIND *results, const char *query, char field_data[][STRING_SIZE], char *result_data) {
+int stmt_prepare(MYSQL *mysql, MYSQL_STMT *stmt, MYSQL_BIND *param, MYSQL_BIND *results, const char *query, char field_data[][STRING_SIZE], char **result_data) {
     int fields;
-    int field_count = mysql_stmt_field_count(stmt);
+    int field_count;
+    int row_count;
     unsigned long *field_length;
     unsigned long *data_length;
     param = 0;
@@ -20,15 +25,31 @@ int stmt_prepare(MYSQL *mysql, MYSQL_STMT *stmt, MYSQL_BIND *param, MYSQL_BIND *
     }
 
     fields = mysql_stmt_param_count(stmt);
+    field_count = mysql_stmt_field_count(stmt);
+
     if (fields > 0) {
         param = (MYSQL_BIND*)malloc(sizeof(MYSQL_BIND) * fields); // @TODO free
         results = (MYSQL_BIND*)malloc(sizeof(MYSQL_BIND) * field_count); // @TODO free
         field_length = (unsigned long *)malloc(sizeof(unsigned long) * fields); //@TODO free
         data_length = (unsigned long *)malloc(sizeof(unsigned long) * field_count); //@TODO free 
-        memset(param, 0, sizeof(MYSQL_BIND) * fields);
-        memset(results, 0, sizeof(MYSQL_BIND) * fields);
-        memset(field_length, 0, sizeof(unsigned long) * fields);
-        memset(data_length, 0, sizeof(unsigned long) * field_count);
+        result_data = malloc(sizeof(char*) * field_count);
+
+        for (int i=0; i<field_count; i++) {
+            result_data[i] = (char *) malloc((STRING_SIZE+1) *  sizeof(char));
+        }
+
+        if (param != NULL) {
+            memset(param, 0, sizeof(MYSQL_BIND) * fields);
+        }
+        if (results != NULL) {
+            memset(results, 0, sizeof(MYSQL_BIND) * field_count);
+        }
+        if (field_length != NULL) {
+            memset(field_length, 0, sizeof(unsigned long) * fields);
+        }
+        if (data_length != NULL) {
+            memset(data_length, 0, sizeof(unsigned long) * field_count);
+        }
     
         for (int i=0; i<fields; i++) {
             param[i].buffer_type = MYSQL_TYPE_STRING;
@@ -39,18 +60,14 @@ int stmt_prepare(MYSQL *mysql, MYSQL_STMT *stmt, MYSQL_BIND *param, MYSQL_BIND *
         
             field_length[i] = strlen(field_data[i]); 
         }
-
-        printf("%lu", sizeof(data_length));
     
         for (int i=0; i<field_count; i++) {
             results[i].buffer_type = MYSQL_TYPE_STRING;
-            results[i].buffer = result_data;
+            results[i].buffer = result_data[i];
             results[i].buffer_length = STRING_SIZE;
             results[i].is_null = 0;
             results[i].length = &data_length[i];
         }
-
-        printf("results");
 
         if (mysql_stmt_bind_param(stmt, param)) {
             fprintf(stderr, "mysql_stmt_bind_param() failed\n");
@@ -69,10 +86,19 @@ int stmt_prepare(MYSQL *mysql, MYSQL_STMT *stmt, MYSQL_BIND *param, MYSQL_BIND *
             fprintf(stderr, " %s\n", mysql_stmt_error(stmt));
             exit(EXIT_FAILURE);
         }
-    
-        printf("%d\n", field_count); 
+
+        if (mysql_stmt_store_result(stmt)) {
+            fprintf(stderr, " mysql_stmt_store_result() failed\n");
+            fprintf(stderr, " %s\n", mysql_stmt_error(stmt));
+            exit(EXIT_FAILURE);
+        }
+   
+        row_count = 0;
+
         while (mysql_stmt_fetch(stmt) == 0) {
-            printf("%s\n", result_data); 
+            for (int i=0; i<field_count; i++) {
+                printf("%s\n", result_data[i]); 
+            }
         }
 
         if (mysql_stmt_close(stmt)) {
@@ -81,6 +107,11 @@ int stmt_prepare(MYSQL *mysql, MYSQL_STMT *stmt, MYSQL_BIND *param, MYSQL_BIND *
             exit(EXIT_FAILURE); 
         }
     }
+
+    free(param);
+    free(results);
+    free(field_length);
+    free(data_length);
     
     return 0;
 }
@@ -92,19 +123,19 @@ int main() {
     MYSQL_BIND *param, *results;
     const char *sql;
     char field_data[2][STRING_SIZE];
-    char result_data[STRING_SIZE];
+    char **result_data;
 
-    strncpy(field_data[0], "andrew", STRING_SIZE);
+    strncpy(field_data[0], "adrek", STRING_SIZE);
     strncpy(field_data[1], "2", STRING_SIZE);
 
     mysql = mysql_init(NULL);
 
-    if (mysql_real_connect(mysql, "localhost", "", "", "", 0, NULL, 0) == NULL) {
+    if (mysql_real_connect(mysql, DB_HOST, DB_USER, DB_PASS, DB_NAME, 0, NULL, 0) == NULL) {
         fprintf(stderr, "No Connection could be made to the database");
         exit(EXIT_FAILURE);
     } else {
         printf("Connection Established...\n");
-        sql = "SELECT `username`, `password`  FROM Users WHERE username = ? or user_id = ?";
+        sql = "SELECT `username`, `password`, `email`  FROM Users WHERE username = ? or user_id = ?";
         stmt_prepare(mysql, stmt, param, results, sql, field_data, result_data);
     }
 
