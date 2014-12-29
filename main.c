f9c2713015046db72d293f0b08aaa765473003eb
Andrew Jordan <acjordan2@gmail.com>
2014-12-29 17:05:14 -0600

    created header files and demo

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <mysql/mysql.h>
+#include "stmt_prepare.h"
+
+#define DB_HOST  ""
+#define DB_USER  ""
+#define DB_PASS  ""
+#define DB_NAME  ""
+
+int main() {
+    MYSQL *mysql;
+    MYSQL_STMT *stmt;
+    const char *query;
+    char field_data[2][STRING_SIZE];
+    unsigned long *field_length, *data_length; 
+    char **result_data;
+
+    strncpy(field_data[0], "adrek", STRING_SIZE);
+    strncpy(field_data[1], "2", STRING_SIZE);
+
+    if (mysql_library_init(0, NULL, NULL)) {
+        fprintf(stderr, "could not initialize MySQL library\n");
+        return -1;
+    } 
+
+    mysql = mysql_init(NULL);
+    
+    if (mysql_real_connect(mysql, DB_HOST, DB_USER, DB_PASS, DB_NAME, 0, NULL, 0) == NULL) {
+        fprintf(stderr, "No Connection could be made to the database");
+        exit(EXIT_FAILURE);
+    } else {
+        query =  "SELECT `username`, `password`, `email`  FROM Users WHERE user_id > 1";
+        if ((stmt = mysql_stmt_init(mysql)) == 0) {
+            return -1;
+        }
+
+        // Set up prepared statements, memory is dynmically allocated for the parmeter and results buffers
+        // These will need to be freed using stmt_free
+        if ((stmt_prepare(stmt, query, field_data, &field_length, &result_data, &data_length)) != 0) {
+            return -1;
+        }
+        mysql_stmt_execute(stmt);
+        
+        // Print results, 1 row at a time
+        while (mysql_stmt_fetch(stmt) == 0) {
+           for (int i=0; i<mysql_stmt_field_count(stmt); i++) {
+                printf("%s\n", result_data[i]); 
+           }
+        }
+        
+        // Free memory
+        mysql_stmt_free_result(stmt);
+        stmt_free(&result_data, mysql_stmt_field_count(stmt), &field_length, &data_length);
+        mysql_stmt_close(stmt);
+       
+        mysql_close(mysql);
+        mysql_library_end();
+    }
+}
+
--- stmt_prepare.c
+++ stmt_prepare.c
@@ -3,12 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <mysql/mysql.h>
-
-#define STRING_SIZE 256
-#define DB_HOST  ""
-#define DB_USER  ""
-#define DB_PASS  ""
-#define DB_NAME  ""
+#include "stmt_prepare.h"
 
 int stmt_prepare(MYSQL_STMT *stmt, const char *query, char field_data[][STRING_SIZE], unsigned long **field_length, char ***result_data, unsigned long **data_length) {
     long unsigned int param_count,
@@ -18,6 +13,7 @@ int stmt_prepare(MYSQL_STMT *stmt, const char *query, char field_data[][STRING_S
                *results;
 
     if (mysql_stmt_prepare(stmt, query, strlen(query)) != 0) {
+        fprintf(stderr, "%s\n", mysql_stmt_error(stmt));
         return -1;
     }
 
@@ -97,53 +93,3 @@ void stmt_free(char ***result_data, int result_data_size,  unsigned long **field
     (*data_length) = NULL;
 }
 
-int main() {
-    MYSQL *mysql;
-    MYSQL_STMT *stmt;
-    const char *query;
-    char field_data[2][STRING_SIZE];
-    unsigned long *field_length, *data_length; 
-    char **result_data;
-
-    strncpy(field_data[0], "adrek", STRING_SIZE);
-    strncpy(field_data[1], "2", STRING_SIZE);
-
-    if (mysql_library_init(0, NULL, NULL)) {
-        fprintf(stderr, "could not initialize MySQL library\n");
-        exit(EXIT_FAILURE);
-    } 
-
-    mysql = mysql_init(NULL);
-    
-    if (mysql_real_connect(mysql, DB_HOST, DB_USER, DB_PASS, DB_NAME, 0, NULL, 0) == NULL) {
-        fprintf(stderr, "No Connection could be made to the database");
-        exit(EXIT_FAILURE);
-    } else {
-        query =  "SELECT `username`, `password`, `email`  FROM Users WHERE username = ? OR user_id = ?";
-        if ((stmt = mysql_stmt_init(mysql)) == 0) {
-            return -1;
-        }
-
-        // Set up prepared statements, memory is dynmically allocated for the parmeter and results buffers
-        // These will need to be freed using stmt_free
-        if ((stmt_prepare(stmt, query, field_data, &field_length, &result_data, &data_length)) != 0) {
-            return -1;
-        }
-        mysql_stmt_execute(stmt);
-        
-        // Print results, 1 row at a time
-        while (mysql_stmt_fetch(stmt) == 0) {
-           for (int i=0; i<mysql_stmt_field_count(stmt); i++) {
-                printf("%s\n", result_data[i]); 
-           }
-        }
-        
-        // Free memory
-        mysql_stmt_free_result(stmt);
-        stmt_free(&result_data, mysql_stmt_field_count(stmt), &field_length, &data_length);
-        mysql_stmt_close(stmt);
-       
-        mysql_close(mysql);
-        mysql_library_end();
-    }
-}
